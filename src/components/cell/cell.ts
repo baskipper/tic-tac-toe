517a096f578db7f459afc2f68d4e517169f2af4e
BSkipper <bskipper@commandalkon.com>
2018-02-19 19:21:38 -0600

    X and O icons now alternate

--- src/components/cell/cell.ts
+++ src/components/cell/cell.ts
@@ -11,21 +11,25 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: 'cell.html'
 })
 export class CellComponent {
-
-  text: string;
-  temporaryHide: boolean = true;
-  hasBeenClicked: boolean = false;
   @Input('player') player: string = 'empty';
   @Output('change') change = new EventEmitter();
 
+  value: string = 'empty';
+  private X: string = 'X';
+  private O: string = 'O';
+  temporaryHide: boolean = true;
+  hasBeenClicked: boolean = false;
+
+
   constructor() {
     console.log('Hello CellComponent Component');
-    this.text = 'Hello World';
+    // this.text = 'Hello World';
   }
 
   onClick() {
   if(!this.hasBeenClicked) {
     console.log('Cell Clicked! ', this.player);
+    this.value = this.player;
     this.temporaryHide = false;
     this.hasBeenClicked = true;
     this.change.emit();
